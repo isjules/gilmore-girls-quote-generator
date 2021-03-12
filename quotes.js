302077565fbd7aedb5285258fdd6c70110bf8ba3
isjules <72038937+isjules@users.noreply.github.com>
2021-03-12 13:09:02 -0800

    Add files via upload

---
+++ quotes.js
@@ -0,0 +1,66 @@
+const localQuotes = [
+  {
+    text: 'This thing we’re doing here — you, me — I’m in. I’m all in.',
+    author: 'Luke',
+  },
+  {
+    text: 'People are particularly stupid today. I can’t talk to any more of them.',
+    author: 'Michel',
+  },
+  {
+    text: 'If it was physically possible to make love to a hot beverage, this would be the one.',
+    author: 'Lorelai',
+  },
+  {
+    text: 'Oy with the poodles already!',
+    author: 'Lorelai',
+  },
+  {
+    text: 'This is a jumbo coffee morning. I need coffee in an I.V."',
+    author: 'Lorelai',
+  },
+  {
+    text: 'I smell snow.',
+    author: 'Lorelai',
+  },
+  {
+    text: 'You jump, I jump, Jack!',
+    author: 'Rory',
+  },
+  {
+    text: 'I live in two worlds, one is a world of books.',
+    author: 'Rory',
+  },
+  {
+    text: 'I love you, you idiot.',
+    author: 'Rory',
+  },
+  {
+    text: 'I stop drinking coffee, I stop doing the standing and walking and the words-putting-into-sentence-doing.',
+    author: 'Lorelai',
+  },
+  {
+    text: 'Flirt with him, now. We need coffee.',
+    author: 'Rory',
+  },
+  {
+    text: 'Plus you know you can get a great cup of coffee in New York.',
+    author: 'Lorelai',
+  },
+  {
+    text: 'Coffee, please, and a shot of cynicism.',
+    author: 'Lorelai',
+  },
+  {
+    text: 'Everything in my life has something to do with coffee. I believe in a former life, I was coffee.',
+    author: 'Lorelai',
+  },
+  {
+    text: 'You know what, three coffees would be great.',
+    author: 'Lorelai',
+  },
+  {
+    text: 'You drank all the coffee in your house before 5 in the morning',
+    author: 'Luke',
+  },
+];
